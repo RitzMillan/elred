ffc86d32c977a7d62553127e711592352361a17b
Rishabh <rishabh@opinverse.com>
2023-05-17 10:34:01 +0530

    updates

--- src/Components/Popup/Popup.js
+++ src/Components/Popup/Popup.js
@@ -32,86 +32,88 @@ function Popup(props) {
 
   return (
     <>
-      <div className="popup-container">
-        <div className="left">
-          <div className="header">
-            <p className="f-22">
-              <b>{props.item.itemDescription}</b>
-            </p>
-          </div>
-          <div className="left-body-container">
-            <div className="left-body">
-              <div
-                className="big-product-img"
-                style={{
-                  backgroundImage: `url(${productImg})`,
-                }}
-              >
-                <img src={heart} alt="like" className="heart" />
-              </div>
-              <p className="text-grey f-12">#000789</p>
-              <div className="name-price">
-                <p className="f-16">
-                  <b>{props.item.itemDescription}</b>
+      <div className="popup-container-wraper">
+        <div className="popup-container">
+          <div className="left">
+            <div className="header">
+              <p className="f-22">
+                <b>{props.item.itemDescription}</b>
+              </p>
+            </div>
+            <div className="left-body-container">
+              <div className="left-body">
+                <div
+                  className="big-product-img"
+                  style={{
+                    backgroundImage: `url(${productImg})`,
+                  }}
+                >
+                  <img src={heart} alt="like" className="heart" />
+                </div>
+                <p className="text-grey f-12">#000789</p>
+                <div className="name-price">
+                  <p className="f-16">
+                    <b>{props.item.itemDescription}</b>
+                  </p>
+                  <p className="f-16">
+                    <b>{props.item.currency.symbol}300</b>
+                  </p>
+                </div>
+                <p className="f-10 text-grey product-description-text">
+                  kuh adkha dakisdj suhe akudh asdkj d aoidh ad adi adia daid
+                  oasid adiu aqofiqep uwef wef pwe fwe we wp uwpgu wp gpwr gwr{" "}
                 </p>
-                <p className="f-16">
-                  <b>{props.item.currency.symbol}300</b>
+                <p className="f-14 color-description">
+                  Please Select Color Description
                 </p>
-              </div>
-              <p className="f-10 text-grey product-description-text">
-                kuh adkha dakisdj suhe akudh asdkj d aoidh ad adi adia daid
-                oasid adiu aqofiqep uwef wef pwe fwe we wp uwpgu wp gpwr gwr{" "}
-              </p>
-              <p className="f-14 color-description">
-                Please Select Color Description
-              </p>
-              <div className="cell-container">
-                {props.item.variants.map((item, i) => (
-                  <Cell1 key={i} item={item} />
-                ))}
-              </div>
-              <p className="f-14 color-description">
-                Please Select Packaging Description
-              </p>
-              <div className="cell-container">
-                {props.item.variants.map((item, i) => (
-                  <Cell2 key={i} item={item} />
-                ))}
-              </div>
-              <p className="f-14 color-description">Enter Quantity</p>
-              <input type="text" className="qty-input" />
-              <p className="f-10 text-red">Minimum orders 12*</p>
-              <label className="checkbox">
-                Need Urgent order
-                <input type="checkbox" />
-                <span className="checkmark"></span>
-              </label>
-              <div className="button-wraper" onClick={handleAdd}>
-                <button className="btn-outline">ADD</button>
+                <div className="cell-container">
+                  {props.item.variants.map((item, i) => (
+                    <Cell1 key={i} item={item} />
+                  ))}
+                </div>
+                <p className="f-14 color-description">
+                  Please Select Packaging Description
+                </p>
+                <div className="cell-container">
+                  {props.item.variants.map((item, i) => (
+                    <Cell2 key={i} item={item} />
+                  ))}
+                </div>
+                <p className="f-14 color-description">Enter Quantity</p>
+                <input type="text" className="qty-input" />
+                <p className="f-10 text-red">Minimum orders 12*</p>
+                <label className="checkbox">
+                  Need Urgent order
+                  <input type="checkbox" />
+                  <span className="checkmark"></span>
+                </label>
+                <div className="button-wraper" onClick={handleAdd}>
+                  <button className="btn-outline">ADD</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="v-divider" />
-        <div className="right">
-          <div className="header">
-            <p className="f-22">
-              <b>Order List</b>
-            </p>
-            <img src={close} alt="close" onClick={handlePopupClose} />
-          </div>
-          <div className="header-two">
-            <p className="pro">Products</p>
-            <p className="qty">QTY</p>
-            <p className="pri">Price</p>
-          </div>
-          <div className="cartitems-wraper">
-            {orderList.map((item, i) => (
-              <CartItems key={i} item={item} />
-            ))}
-          </div>
-          <div className="btn-container" onClick={handleAddToCart}>
-            <button className="btn-primary">Add to cart</button>
+          <div className="v-divider" />
+          <div className="right">
+            <div className="header">
+              <p className="f-22">
+                <b>Order List</b>
+              </p>
+              <img src={close} alt="close" onClick={handlePopupClose} />
+            </div>
+            <div className="header-two">
+              <p className="pro">Products</p>
+              <p className="qty">QTY</p>
+              <p className="pri">Price</p>
+            </div>
+            <div className="cartitems-wraper">
+              {orderList.map((item, i) => (
+                <CartItems key={i} item={item} />
+              ))}
+            </div>
+            <div className="btn-container" onClick={handleAddToCart}>
+              <button className="btn-primary">Add to cart</button>
+            </div>
           </div>
         </div>
       </div>
@@ -125,7 +127,9 @@ export function Cell1(props) {
   return (
     <>
       <div className="cell-wraper">
-        <div className="cell-name selected-cell" tabIndex={1}>{props.item.colorDescription}</div>
+        <div className="cell-name selected-cell" tabIndex={1}>
+          {props.item.colorDescription}
+        </div>
       </div>
     </>
   );
@@ -135,7 +139,9 @@ export function Cell2(props) {
   return (
     <>
       <div className="cell-wraper">
-        <div className="cell-name selected-cell" tabIndex={1}>{props.item.packingDescription}</div>
+        <div className="cell-name selected-cell" tabIndex={1}>
+          {props.item.packingDescription}
+        </div>
       </div>
     </>
   );
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -7,25 +7,33 @@ import heart from "../../Assets/Icons/heart.svg";
 import Popup from "../Popup/Popup";
 
 function Product1(props) {
-
   const item = props.item;
 
-  const [popup, setPopup] = useState(false)
+  const [popup, setPopup] = useState(false);
   const img = props.item.productImages[0];
   const productImg = img !== undefined ? img : product;
   // const dispatch = useDispatch();
 
   function handleClick() {
     // dispatch(popup(true));
-    setPopup(true)
+    setPopup(true);
   }
 
   return (
     <>
-      <div className="product-container selected-border" onClick={handleClick} tabIndex={1}>
+      <div
+        className="product-container selected-border"
+        onClick={handleClick}
+        tabIndex={1}
+      >
         <img src={heart} alt="like" className="heart" />
         <div className="img-container">
-          <img src={productImg} alt="product" className="product-img" />
+          <div
+            style={{
+              backgroundImage: `url(${productImg})`,
+            }}
+            className="product-img"
+          />
         </div>
         <div className="text-container">
           <p className="f-12">
